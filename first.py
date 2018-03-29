917aca1cac54cc0c4bade28ab58f6693801f5e85
WinSun1986 <810264@qq.com>
2018-03-29 15:59:05 +0800

    test

--- first.py
+++ first.py
@@ -4,6 +4,4 @@ lst = glob.glob('/home/ws/img/*/*/*.jpg')
 for l in lst:
   print(l)
 
-sdfk
-
-shf[iash
+https://d324634f61522a6a517c3ff27d7159ede10c3679@github.com/chiliai/clips-data-images-Amount2.git
